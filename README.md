050ada58101e24b4d16621113ddb90167d43c368
FilterdWater <129171301+FilterdWater@users.noreply.github.com>
2025-08-29 14:30:33 +0200

    My first C program

---
+++ README.md
@@ -0,0 +1,5 @@
+# Bro Code's C Course
+
+This repo contains the code and commits written while following the Bro Code C Course for fun.
+
+[Course Link](https://www.youtube.com/watch?v=xND0t1pr3KY)
---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+
+    // This is my first program in C! :)
+
+    /*
+    This
+    is
+    also
+    a
+    comment
+    */
+
+    printf("I like pizza!\n");
+    printf("It's really good!");
+
+    return 0;
+}
