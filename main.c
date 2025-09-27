00b673b533bb6eff056c6ac247b56fc0de3f3073
FilterdWater <129171301+FilterdWater@users.noreply.github.com>
2025-09-27 10:07:56 +0200

    Format specifiers

--- main.c
+++ main.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
-#include <stdbool.h>
 
 int main()
 {
-    bool isOnline = false;
+    int age = 25;
+    float price = 19.99;
+    double pi = 3.1415926535;
+    char currency[] = "€"; // char stores 1 byte, € is 3 bytes, therefore a string was used instead
+    char name[] = "FilterdWater";
 
-    if (isOnline)
-    {
-        printf("Your are online");
-    }
-    else
-    {
-        printf("You are OFFLINE");
-    }
+    printf("%d\n", age);
+    printf("%f\n", price);
+    printf("%lf\n", pi);
+    printf("%s\n", currency);
+    printf("%s\n", name);
+
+    float price1 = 10.99;
+    float price2 = 1.50;
+    float price3 = -100.00;
+
+    printf("%+7.2f\n", price1);
+    printf("%+7.2f\n", price2);
+    printf("%+7.2f\n", price3);
 
     return 0;
 }
