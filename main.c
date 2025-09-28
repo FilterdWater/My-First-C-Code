7e03db4d9e84732068282e7e65801f8c29ff6de5
FilterdWater <129171301+FilterdWater@users.noreply.github.com>
2025-09-28 12:20:33 +0200

    Arithmetic operators

--- main.c
+++ main.c
@@ -2,25 +2,22 @@
 
 int main()
 {
-    int age = 25;
-    float price = 19.99;
-    double pi = 3.1415926535;
-    char currency[] = "€"; // char stores 1 byte, € is 3 bytes, therefore a string was used instead
-    char name[] = "FilterdWater";
+    int x = 10;
+    int y = 2;
+    int z = 0;
 
-    printf("%d\n", age);
-    printf("%f\n", price);
-    printf("%lf\n", pi);
-    printf("%s\n", currency);
-    printf("%s\n", name);
+    // z = x + y;
+    // z = x - y;
+    // z = x * y;
+    // z = x / y;
+    // z = x % y;
 
-    float price1 = 10.99;
-    float price2 = 1.50;
-    float price3 = -100.00;
+    // x += 2;
+    // x -= 3;
+    // x *= 2;
+    // x /= 2;
 
-    printf("%+7.2f\n", price1);
-    printf("%+7.2f\n", price2);
-    printf("%+7.2f\n", price3);
+    printf("%d", x);
 
     return 0;
 }
